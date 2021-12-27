6c2e014193ea464e1d1af0101a6e8803db56f3a5
LukGaming <thelukgaming333@gmail.com>
2021-12-27 15:05:28 -0300

    Animated Css library

--- animacoes-exercicios/src/App.vue
+++ animacoes-exercicios/src/App.vue
@@ -4,9 +4,23 @@
     <b-button variant="primary" class="mb-4" @click="ShowMessage = !ShowMessage"
       >Mostrar Mensagem</b-button
     >
-    <transition name="fade">
+    <!-- <transition name="fade" appear>
       <b-alert show v-if="ShowMessage">Default Alert</b-alert>
     </transition>
+    <transition name="slide" appear="">
+      <b-alert show v-if="ShowMessage">Default Alert</b-alert>
+    </transition>
+	<transition enter-active-class="animated bounce" leave-active-class="animated shakeX">
+      <b-alert show v-if="ShowMessage">Default Alert</b-alert>
+    </transition> -->
+	<transition :name="ChooseTypeOfAnimation" appear>
+      <b-alert show v-if="ShowMessage">Default Alert</b-alert>
+    </transition>
+	<hr>
+	<b-select v-model="ChooseTypeOfAnimation">
+		<option value="fade">Fade</option>
+		<option value="slide">Slide</option>
+	</b-select>
   </div>
 </template>
 
@@ -16,6 +30,7 @@ export default {
     return {
       msg: "Olá mundo!",
       ShowMessage: true,
+	  ChooseTypeOfAnimation: 'fade'
     };
   },
 };
@@ -31,11 +46,41 @@ export default {
   margin-top: 60px;
   font-size: 1.5rem;
 }
-.fade-enter, .fade-leave {
+.fade-enter,
+.fade-leave {
   opacity: 0;
 }
-.fade-enter-active, .fade-leave-active {
-	transition: opacity 2s;
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 2s;
+}
+@keyframes slide-in {
+  from {
+    transform: translateX(4000px);
+  }
+  to {
+    transform: translateX(0);
+  }
+}
+@keyframes slide-out {
+  from {
+    transform: translateX(4000px);
+  }
+  to {
+    transform: translateX(0);
+  }
 }
 
+.slide-enter-active {
+  animation: slide-in 1s ease;
+  transition: opacity 1s;
+}
+.slide-leave-active {
+  animation: slide-out 1s ease;
+  transition: opacity 1s;
+}
+.slide-enter,
+.slide-leave {
+  transform: opacity 2s;
+}
 </style>
