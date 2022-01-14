3fb606f5d29df1edc78c80b2c0b91828eb784097
LukGaming <thelukgaming333@gmail.com>
2022-01-14 11:17:05 -0300

    Last Update Before Resource

--- router/src/modules/user/get_user_data.js
+++ router/src/modules/user/get_user_data.js
@@ -1,6 +1,7 @@
 export default {
     async get_user_data($user_id) {
-        return this.$http.get(`api/user/${$user_id}`).then(res => {
+        return this.$http.get(`api/user/${$user_id}`).then(res => res.json()).then( res =>{ 
+            console.log(res.json(0))
             return res.data;
         }).catch(error => {
             return error;
